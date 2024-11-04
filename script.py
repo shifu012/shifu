792f2d23f3e4889b62ee30e75ab8e8f1d4ef1988
shifu012 <taha.nasa000@gmail.com>
2024-11-04 19:38:06 +0330

    Add files via upload

---
+++ script.py
@@ -0,0 +1,31 @@
+from os import system
+from colorama import Fore
+feom time import sleep
+system ("clear")
+
+print ("hello world")
+print ("the script filtring_error")
+print ("sakhte shode tavasot gorooh hiroshima")
+code = input (f"""
+        
+              (1) bug
+              (2) algoritm
+              (3) link sexy
+              (4) matn filter account
+              (5) link mokhareb      
+              """)
+print("")
+print (fore.GREEN+"loading...")
+sleep (10)
+if code == "1":
+ print(fore.RED+"online.ofline.com")
+if code == "2":
+ print(fore.RED+"https://1.0.0.4.3.8.8.8.7.2.2.3.1.0.0.0.0.9.9.9.9.9.3.4.4.4.4.0.1.4.7.9.8.spam.ir")
+        
+
+if code == "3":
+ print (fore.RED+"www.pornhub.com")
+if code == "4":
+ print (fore.RED+"This account is dangerous in every way, please, this account is like hacker or terrorist accounts")
+if code == "5":
+ print(fore.RED+"https://darkweb-sites.org/?s=https%3A%2F%2Ffiltering-rubika.com")
